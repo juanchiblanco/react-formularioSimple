b8f74486cb9bcc9beab3c1bbd921ea1b128d9cfb
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-22 19:56:32 -0300

    agregue el componente Form de reactbootstrap en mi componente Formulario

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,33 @@
-
+import Button from "react-bootstrap/Button";
+import Form from "react-bootstrap/Form";
 
 const Formulario = () => {
-    return (
-        <section>
-            
-        </section>
-    );
+  return (
+    <section className="row justify-content-center mt-5">
+      <div className="bg-light shadow p-3 shadow rounded">
+        <Form>
+          <Form.Group className="mb-3" controlId="formBasicEmail">
+            <Form.Label>Email address</Form.Label>
+            <Form.Control type="email" placeholder="Enter email" />
+            <Form.Text className="text-muted">
+              We'll never share your email with anyone else.
+            </Form.Text>
+          </Form.Group>
+
+          <Form.Group className="mb-3" controlId="formBasicPassword">
+            <Form.Label>Password</Form.Label>
+            <Form.Control type="password" placeholder="Password" />
+          </Form.Group>
+          <Form.Group className="mb-3" controlId="formBasicCheckbox">
+            <Form.Check type="checkbox" label="Check me out" />
+          </Form.Group>
+          <Button variant="primary" type="submit">
+            Submit
+          </Button>
+        </Form>
+      </div>
+    </section>
+  );
 };
 
 export default Formulario;
