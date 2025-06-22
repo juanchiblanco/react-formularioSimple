7d81811069770bee53d06860c498f93754a57ad8
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-22 19:49:48 -0300

    agregue el footer en app, lo baj[e y cambi[e el color de fondo del body

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,16 @@
-function App() {
+import "bootstrap/dist/css/bootstrap.min.css";
 
+function App() {
   return (
     <>
-      <h1>Hola Mundo</h1>
+      <main>
+        <h1>Hola Mundo</h1>
+      </main>
+      <footer className="bg-light text-dark text-center py-3">
+        <p>&copy;Todos los derechos reservados</p>
+      </footer>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
