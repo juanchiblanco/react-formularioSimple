c7ae74c47d123bab66c03a794ad5aa02ec9b323a
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-23 22:58:52 -0300

    importe react hook form y agregue handlesubmit

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -1,11 +1,20 @@
 import Button from "react-bootstrap/Button";
 import Form from "react-bootstrap/Form";
+import { useForm } from "react-hook-form";
 
 const Formulario = () => {
+
+  const {
+    register,
+    handleSubmit,
+    reset,
+    formState: { errors },
+  } = useForm();
+
   return (
     <section className="row justify-content-center mt-5">
       <div className="bg-light shadow p-3 shadow rounded w-75">
-        <Form>
+        <Form onSubmit={handleSubmit(reset())}>
           <h5 className="text-center lead border border-dark py-3 rounded">Ingresa tus datos</h5>
           <Form.Group className="mb-3" controlId="nombre">
             <Form.Label>Nombre</Form.Label>
