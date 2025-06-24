b1986c55b46177d2d780ac4f79d1b531ccea1496
Juanchi Blanco <juanchiblancosanpa@gmail.com>
2025-06-23 23:15:14 -0300

    agregue validaciones de input nombre y apellido

--- src/components/Formulario.jsx
+++ src/components/Formulario.jsx
@@ -3,7 +3,6 @@ import Form from "react-bootstrap/Form";
 import { useForm } from "react-hook-form";
 
 const Formulario = () => {
-
   const {
     register,
     handleSubmit,
@@ -14,16 +13,64 @@ const Formulario = () => {
   return (
     <section className="row justify-content-center mt-5">
       <div className="bg-light shadow p-3 shadow rounded w-75">
-        <Form onSubmit={handleSubmit(reset())}>
-          <h5 className="text-center lead border border-dark py-3 rounded">Ingresa tus datos</h5>
+        <Form onSubmit={handleSubmit()}>
+          <h5 className="text-center lead border border-dark py-3 rounded">
+            Ingresa tus datos
+          </h5>
           <Form.Group className="mb-3" controlId="nombre">
             <Form.Label>Nombre</Form.Label>
-            <Form.Control type="name" placeholder="Ej: Juan" required />
+            <Form.Control
+              type="name"
+              placeholder="Ej: Juan"
+              {...register("inputNombre", {
+                required: "El nombre es un dato obligatorio",
+                minLength: {
+                  value: 3,
+                  message: "El nombre debe tener 3 caracteres como minimo",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "El nombre debe tener 50 caracteres como minimo",
+                },
+                pattern: {
+                  value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
+                  message:
+                    "El nombre debe contener caracteres alfabeticos, mayusculas o minusculas",
+                },
+              })}
+              maxLength={60}
+            />
           </Form.Group>
+          <Form.Text className="text-danger">
+            {errors.inputNombre?.message}
+          </Form.Text>
           <Form.Group className="mb-3" controlId="apellido">
             <Form.Label>Apellido</Form.Label>
-            <Form.Control type="name" placeholder="Ej: Perez" required />
+            <Form.Control
+              type="name"
+              placeholder="Ej: Perez"
+              {...register("inputApellido", {
+                required: "El apelido es un dato obligatorio",
+                minLength: {
+                  value: 3,
+                  message: "El apelido debe tener 3 caracteres como minimo",
+                },
+                maxLength: {
+                  value: 50,
+                  message: "El apelido debe tener 50 caracteres como minimo",
+                },
+                pattern: {
+                  value: /^[A-Za-zÁÉÍÓÚáéíóúÑñ\s]+$/,
+                  message:
+                    "El apellido debe contener caracteres alfabeticos, mayusculas o minusculas",
+                },
+              })}
+              maxLength={60}
+            />
           </Form.Group>
+          <Form.Text className="text-danger">
+            {errors.inputApellido?.message}
+          </Form.Text>
           <Form.Group className="mb-3" controlId="dni">
             <Form.Label>DNI</Form.Label>
             <Form.Control type="number" placeholder="Ej: 41256987" required />
